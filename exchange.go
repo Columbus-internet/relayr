ae3206a27ee9d276ec852e8be1ab3e68f195ba90
Anton B <antonb@columbus-internet.com>
2017-06-07 16:19:31 +0700

    adjustments

--- exchange.go
+++ exchange.go
@@ -110,7 +110,7 @@ func extractOperationFromURL(r *http.Request) string {
 func (e *Exchange) upgradeWebSocket(w http.ResponseWriter, r *http.Request) {
 	ws, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		w.Write([]byte(err.Error()))
+		log.Println(err.Error())
 		return
 	}
 	log.Printf("upgradeWebSocket\n")
